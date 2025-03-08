49d3b7580dd70bb27439678867a3acb51cc1834c
Balujavangula <bnarayanararao734@gmail.com>
2025-03-08 12:16:55 +0530

    update backend file

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "mypcgsg"
-    key            = "pcg/terraform.tfstate"
+    key            = "pcg/test/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
