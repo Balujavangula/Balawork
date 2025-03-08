edd465743e75a3ac3238465fca073b5265ad14b2
Balujavangula <bnarayanararao734@gmail.com>
2025-03-08 13:17:27 +0530

    mainfile

--- main.tf
+++ main.tf
@@ -283,7 +283,7 @@ resource "aws_ecr_repository" "pcg_ecr" {
 resource "aws_vpc_endpoint" "ecr_endpoint" {
   vpc_id            = aws_vpc.primary_vpc.id
   service_name      = "com.amazonaws.us-east-1.ecr.dkr"
-  route_table_ids   = [aws_vpc.primary_vpc.public_rt]
+  route_table_ids   = [aws_vpc.primary_vpc.aws_route_table.public_rt.id]
   
   tags = {
     Name = "ecr-vpc-endpoint"
